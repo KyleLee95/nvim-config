b2d1ac91b39c2dbc0e94539c51b7ee68dec5b82c
Kyle Lee <kyle@kylelee.dev>
2023-02-18 15:13:45 -0600

    added coderunner keybinds

--- lua/setup/coderunner.lua
+++ lua/setup/coderunner.lua
@@ -49,3 +49,11 @@ coderunner.setup({
   project_path = "", -- No default path defined
   project = {},
 })
+
+vim.keymap.set('n', '<leader>r', ':RunCode<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rf', ':RunFile<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rft', ':RunFile tab<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rp', ':RunProject<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>rc', ':RunClose<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>crf', ':CRFiletype<CR>', { noremap = true, silent = false })
+vim.keymap.set('n', '<leader>crp', ':CRProjects<CR>', { noremap = true, silent = false })
