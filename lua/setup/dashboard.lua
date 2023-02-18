4ae9971c4fd935282d208ebd04aa6a801b8c08a1
Kyle Lee <kyle@kylelee.dev>
2023-02-18 16:17:04 -0600

    added dashboard acii art

--- lua/setup/dashboard.lua
+++ lua/setup/dashboard.lua
@@ -1,5 +1,4 @@
-local status_ok, dashboard = pcall(require, 'dashboard-nvim')
-
+local status_ok, dashboard = pcall(require, 'dashboard')
 if not status_ok then
 	return
 end
@@ -7,7 +6,39 @@ end
 dashboard.setup({
   theme = 'doom',
   config = {
-    header = {}, --your header
+    header = {
+'kkkkkOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOkkkkkkkkkkkkkkxxxxxxxxxx',
+'kOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO00000000000000000OOOOOOOOOOOOOOOOOOOOOOOOOOOkkkkkkkkkkkkxxxxxxxxxxx',
+'OOOOOOOOOOOOOOOOOOOOOOO00000OOOOkkkxxxdddddddddxxkkOO00000000OOOOOOOOOOOOOOOOOOOkkkkkkkkkkkkkxxxxxxxx',
+'OOOOOOOOOOOOOOO0OOkkxxdddooooollcccc:::::::::::::cccloxkO000000OOOOOOOOOOOOOOOOOkkkkkkkkkkkkkkxxxxxxx',
+'OOOOOOOOOOOOOkdlc:;,,,,,`,:lxkkxdolcc:::::::::::::::;;;:lodxkkOOOOOOOOOOOOOOOOOOOOkkkkkkkkkkkkxxxxxxx',
+'OOOOOOOOO0Odc`;..``````:dk000000OOkdolc::::::;;;;;;;;;;::ccllooddddxxxxxxxdddxxxxxxkkkkkkkkkkkxxxxxxx',
+'OOOOOOO0Od;`......````;oO0000OOOkkxxxxddolc::;;;;;;;;;;;;::cccllllooooollllccccccc:cccloxkkkkkkkxxxxx',
+'OOOOOO0kc``........`.`ck000Okkxxddddoooooooollc::;;;;;;;;;:::cccllllllooollllccccc:::::;;:lxkkkkkkxkxx',
+'OOOOO0k:...`........`lO0OOkxdooollllllllllllllllc:::;;;;;;::::cccllllllloolllllcccc::::::;;okkkkkkxxxx',
+'OOOO0Oc....`....'..'ckOkxdollccccccccccccccccccccc:::::;;;:::::ccccllllllolllllcccc::::::;,:dkkkxkxxx',
+'OO0O0d,....`....``.;xOkdollllllllllllllllllcccccccc::::::::::::cccccllllllllllllcccc::::;`.`lxxxxxxxx',
+'OOO0Oc.`````...`.`lkxollllooooooddddddxxdddddooollllcccc::::::ccccccllllllllllllccc::;`....;oddxxxxx',
+'OOO0kw:````....`.;dxollloooodddxxxxxxkkkkxxkkkkxxxxxddoollccc::ccccccllllllllllllcc;`......,lddddddd',
+'OOO0kc`,,`````..:dolloooddxxxxkkkkkkkkkkkkkkkkkkkkkkkkkxxxddolcccccccllllllllllc;`.........:odddodd',
+'O0O0Ol,,,,,`````:ooooddxxxkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkxddolllcccccllc;,............,loooooo',
+'00000x:,;,,,,```.;loodxxxkkkkkkkOOOOOOOkOOOOOkkkkkkkkkkkkkkkkkkkkkkkkkxxdo:;,...............`:oooooo',
+'0O000Od:;;;;,,,``,lodxxkkkkkOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO0000000Oxo:,...................,looooo',
+'OO0O000xc;;;;;,,`,cdxkkkkOOOOOOOOOOOO000OO0000000000000000000KKK0kdlc,`......................`cooooo',
+'OOOOO000Odc;;;;;;,;okkOOOOO00000000000000000000000000KKKKKK0Oxoc;`............................:ddddo',
+'OOO0OOOOOOkoc::;;;;cxOO0000000000KKKKKKKKKKKKKKKKKKKK0Oxdlc;,................................`cxxxdd',
+'OOOOOOOOOOkkxdolc:;;lk000KKKKKKKKKKKKKKKKK00Okkxdoll:;,`....................................`:dkkxxd',
+'OOOOOOOOOOOkkxdddoc:;:loooddddddddddoollcc:;,,`............................................,coxxxxxx',
+'OOOOOOOOOOkkkkxxdddolc:;,,`````````.....................................................`;clodxxxxxx',
+'OOOOOOOOkkkkOOkkxxddooolc:;;,,,`````````````.........................................`;:cllloxxxxxxx',
+'OOOOOOOOkkkkkOkkkkkxxddoooolc:;,,,,,````````````.................................,;:cllllloddxxxxxxd',
+'OOOOOOOOkkkkkkkkkkkkkxxxddoddddol:;,,,,,,``````````````....................`,;:cloooolllooddddxddddd',
+'OOOOOOOOkkkkkkkkkkkkkkkkkxxxxxxxxxxdolc:;,,,,`````````````........``,,;;:cloodoooolllooodddddddddddd',
+'OOOOOOkkkkkkkkkkkkkkkkkkkkkkkxxkkkkkOOkkxdollcc::::::::;;::::::cclloooddddoooooooooddddooooddddddxxx',
+'OOOOOOkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkOOOOOOOOOOkkkxxxxxxxxxxxxdddddddddooodddddddddddddooooddddxxxxxx',
+'OOOOOOOkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkOOOOOOO0OOOOOOOOkkkkkkxxxxxxxxxxxxxxxxxxdddddddddddddxxxxxxxxx',
+'OOOOOOkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkOOOOOOOOOOOOOOOOOOOOOOOkkkkkkkkxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+	}, --your header
     center = {
       {
         icon = ' ',
@@ -15,18 +46,12 @@ dashboard.setup({
         desc = 'Find File           ',
         desc_hl = 'String',
         key = 'b',
-        keymap = 'SPC f f',
+        keymap = ', f f',
         key_hl = 'Number',
         action = 'lua print(2)'
-      },
-      {
-        icon = ' ',
-        desc = 'Find Dotfiles',
-        key = 'f',
-        keymap = 'SPC f d',
-        action = 'lua print(3)'
       },
     },
     footer = {}  --your footer
   }
 })
+
